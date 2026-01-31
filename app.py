be4c8dcb729fd80e5b1eddfb6d63513644566333
yusufkocak01 <ysfkck@gmail.com>
2026-01-31 20:35:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,21 @@ R2_ACCESS_KEY = os.environ.get("R2_ACCESS_KEY")
 R2_SECRET_KEY = os.environ.get("R2_SECRET_KEY")
 R2_PUBLIC_URL = os.environ.get("R2_PUBLIC_URL")
 
-R2_ENDPOINT = f"https://{R2_ACCOUNT_ID}.r2.cloudflarestorage.com"
+ def get_s3():
+    R2_ACCOUNT_ID = os.environ.get("R2_ACCOUNT_ID")
+    R2_BUCKET = os.environ.get("R2_BUCKET")
+    R2_ACCESS_KEY = os.environ.get("R2_ACCESS_KEY")
+    R2_SECRET_KEY = os.environ.get("R2_SECRET_KEY")
 
-s3 = boto3.client(
+    endpoint = f"https://{R2_ACCOUNT_ID}.r2.cloudflarestorage.com"
+
+    return boto3.client(
+        "s3",
+        endpoint_url=endpoint,
+        aws_access_key_id=R2_ACCESS_KEY,
+        aws_secret_access_key=R2_SECRET_KEY
+    )
+
     "s3",
     endpoint_url=R2_ENDPOINT,
     aws_access_key_id=R2_ACCESS_KEY,
